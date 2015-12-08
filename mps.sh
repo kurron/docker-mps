4bf12848df8ef853dbc6cf80c480b82cf9b5ee9d
Ron Kurr <kurr@kurron.org>
2015-12-08 14:13:12 +0000

    Launches but still needs an icon

--- mps.sh
+++ mps.sh
@@ -2,13 +2,13 @@
 
 CMD="docker run \
        --rm \
-       --name intellij \
+       --name mps \
        --net "host" \
        --env DISPLAY=unix$DISPLAY \
        --user 1000:1000 \
        --volume /tmp/.X11-unix:/tmp/.X11-unix \
        --volume $HOME:/home/developer \
-       kurron/docker-intellij:latest"
+       kurron/docker-mps:latest"
 
 echo $CMD
 $CMD
